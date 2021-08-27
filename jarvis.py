3f8b755a4586074783ca44e3ec00a8b0c4bfaddc
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2021-08-27 15:13:11 +0500

    First Commit
    added jarvis.py

---
+++ jarvis.py
@@ -0,0 +1,104 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+from smtplib import SMTP
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 2
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-uk')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('your email', 'your password')
+    server.sendmail('your email', to, content)
+    server.close()
+x = True
+
+if __name__ == "__main__":
+    
+    wishMe()
+    
+    while x:
+        query = takeCommand().lower()
+        speak("What do you need again sir")
+        # Logic for executing tasks based on query
+        if 'open notepad' in query:
+            npath = "C:\\Windows\\System32\\notepad.exe"
+            os.startfile(npath)
+        elif 'open google' in query:
+            npath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+            os.startfile(npath)
+        elif 'open telegram' in query:
+            npath = "C:\\Users\\1\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe"
+            os.startfile(npath)
+        elif 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+        elif 'open youtube' in query:
+            webbrowser.open("www.youtube.com")
+        elif 'search' in query:
+            speak("sir, what should i search on google")
+            cm = takeCommand().lower()
+            webbrowser.open(f"{cm}")
+        elif 'open sublime' in query:
+            npath = "C:\\Program Files\\Sublime Text 3\\sublime_text.exe"
+            os.startfile(npath)  
+        elif 'stop' in query:
+            speak('Ok Sir I\'m stopping myself')
+            x = False
+        elif 'open pycharm' in query:
+            npath = "C:\\Program Files\\JetBrains\\PyCharm Community Edition 2021.2\\bin\\pycharm64.exe"
+            os.startfile(npath)
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+            print(f'the time is {strTime}')
